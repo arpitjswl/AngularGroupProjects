46cc4737670c2a178b46da4a8fe07a927e658897
Arpit Jaiswal <jswl.arpit@gmail.com>
2021-06-15 14:27:02 +0530

    adding some code

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,11 @@ export class UserService {
   url = 'http://localhost:8888/api'; // Default URL for API
 
   constructor(private http : HttpClient) { }
+
+  // Method to Get All Registered Users in DB
+  public getAllUsers(){
+    return this.http.get<User[]>(this.url + '/' + 'getAllUsers');
+  }
 
   // Method to Save data in Database for User Object while registering
   public registerUser(user : User){
@@ -26,5 +32,4 @@ export class UserService {
    public checkForDuplicateEmail(email : string){
     return this.http.get<boolean>(this.url + '/' + 'getEmail' + '/' +email);
   }
-
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user';
 import { AuthenticateService } from '../service/authenticate.service';
 import { ExchangeDataBetweenComponentsService } from '../service/exchange-data-between-components.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,12 +12,28 @@ import { ExchangeDataBetweenComponentsService } from '../service/exchange-data-b
 export class WelcomeComponent implements OnInit {
 
   userName! : string;
-  
+  user! : User[];
+
   constructor(private authenticateService : AuthenticateService,
-    private exchangeDataBetweenComponentsService : ExchangeDataBetweenComponentsService) { }
+    private exchangeDataBetweenComponentsService : ExchangeDataBetweenComponentsService,
+    private userService : UserService) { }
 
   ngOnInit() : void{
     this.userName = this.exchangeDataBetweenComponentsService.getMessage();
   }
 
+  getAllRegsiteredUsers() : User[]{
+    this.userService.getAllUsers().subscribe(
+      res => {
+        this.user = res;
+        return this.user;
+      }
+    );
+
+    return this.user;
+  }
+
+  sort(){
+    this.user.sort((a, b) => a.user_name.localeCompare(b.user_name));
+  }
 }

b6c3e5eb97c50f7b94c0081a6d2f43ffdc300205
Arpit Jaiswal <jswl.arpit@gmail.com>
2021-06-15 14:28:16 +0530

    removing extra imports

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
-import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
